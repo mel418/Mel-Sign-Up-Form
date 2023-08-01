4012617be4f1555ab6dedbfeded2ce3f90b3bb05
mel148 <melodygatan@gmail.com>
2023-08-01 12:40:50 -0700

    couldnt figure out the span click event

---
+++ script.js
@@ -0,0 +1,11 @@
+// // Get references to the elements
+// const spanElement = document.getElementsByTagName("span");
+// const inputField = document.getElementsByTagName('input');
+
+// console.log(inputField)
+
+// // Add a click event listener to the span element
+// spanElement.addEventListener('click', function() {
+//   // When the span is clicked, focus on the input field
+//   inputField.focus();
+// });
